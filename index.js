3616394a73f71fbd3ccb2bb58d534ef6d3344f9c
capable-daemon-6664 <hello@isabelklee.co>
2019-07-04 03:07:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -45,3 +45,17 @@ function sayHiToGrandma(string) {
   if (string === "I love you, Grandma.");
   return "I love you, too.";
 }
+
+
+
+
+
+function sayHiToGrandma(string) {
+const sayHiToGrandma = string;
+  switch (sayHiToGrandma) {
+    case sayHiToGrandma === "hello":
+    return "I can't hear you!";
+    case sayHiToGrandma === "HELLO":
+    return "YES INDEED!";
+  }
+}
