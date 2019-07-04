dcc4f08ec38d78fac8a4dc8c2977ece2554aa8a1
capable-daemon-6664 <hello@isabelklee.co>
2019-07-04 02:45:33 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,55 @@
+function shout(string) {
+  return string.toUpperCase ()
+}
+
+function whisper(string) {
+  return string.toLowerCase ()
+}
+
+function logShout(string) {
+  const spy = `hello`;
+  console.log (spy.toUpperCase());
+}
+
+function logWhisper(string) {
+  const spy = `HELLO`;
+  console.log (spy.toLowerCase());
+}
+
+
+/* this is all grandma msgs
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase);
+  return `I can't hear you!`;
+}
+
+function sayHiToGrandma(string) {
+  if (string.isUpperCase);
+  return `YES INDEED!`;
+}
+
+
+}
+
+*/
+
+/// new grandma functions
+function sayHiToGrandma(string) {
+let sayHiToGrandma = "hello";
+  switch (sayHiToGrandma) {
+    case "hello": console.log ("I can't hear you!");
+    break;
+    case "HELLO": console.log ("YES INDEED!");
+    break;
+    case "I love you, Grandma.": console.log ("I love you, too.");
+    break;
+  }}
+
+function sayHiToGrandma(string) {
+  if (string === `I love you, Grandma.`);
+  return `I love you, too.`;
+}
+
+
+
